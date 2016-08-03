3fcdceb3cc5aa4589c27febdc799d9cfd69c3012
Johan Ribom <jribom@kth.se>
2016-08-03 16:08:47 +0200

    Copied files from the server branch to a new git project.

---
+++ gobletServer.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+    "net/http"
+    "os"
+)
+
+func main() {
+    http.HandleFunc("/", htmlHandler)
+    http.ListenAndServe(":9090", nil)
+}
+
+func IsDirectory(path string) (bool, error) {
+    fileInfo, err := os.Stat(path)
+    return fileInfo.IsDir(), err
+}
+
+func htmlHandler(w http.ResponseWriter, r *http.Request) {
+    file := "pages" + r.URL.Path
+    fileInfo, err := os.Stat(file)
+    if err != nil {
+        return 
+    } else if fileInfo.IsDir() {
+        if file[len(file) - 1:] != "/"{
+            file += "/"
+        }
+        file += "page.html"
+    }
+    http.ServeFile(w, r, file)
+}
