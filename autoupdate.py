0ee69905545182de1465086d6dcab8001dd5f8b6
Johan Ribom <jribom@kth.se>
2016-09-01 16:10:45 +0200

    Added logging to autoupdate.

--- autoupdate.py
+++ autoupdate.py
@@ -1,12 +1,19 @@
 #!/usr/bin/python
 import subprocess, time
-while True:
-    subprocess.Popen(["git", "remote", "update"], stdout=subprocess.PIPE).communicate()[0]
-    local = subprocess.Popen(["git", "rev-parse", r"@"], stdout = subprocess.PIPE).communicate()[0]
-    remote = subprocess.Popen(["git", "rev-parse", r"@{u}"], stdout = subprocess.PIPE).communicate()[0]
-    base = subprocess.Popen(["git", "merge-base", "@", r"@{u}"], stdout = subprocess.PIPE).communicate()[0]
-    if local == remote:
-        time.sleep(30)
-    elif local == base:
-        print "Pulling changes."
-        print subprocess.Popen(["git", "pull"], stdout = subprocess.PIPE).communicate()[0]
+f = open("update.log", 'w')
+try:
+    while True:
+        subprocess.Popen(["git", "remote", "update"], stdout=subprocess.PIPE).communicate()[0]
+        local = subprocess.Popen(["git", "rev-parse", r"@"], stdout = subprocess.PIPE).communicate()[0]
+        remote = subprocess.Popen(["git", "rev-parse", r"@{u}"], stdout = subprocess.PIPE).communicate()[0]
+        base = subprocess.Popen(["git", "merge-base", "@", r"@{u}"], stdout = subprocess.PIPE).communicate()[0]
+        if local == remote:
+            time.sleep(30)
+            f.write("No updates.")
+        elif local == base:
+            f.write("Pulling changes.")
+            f.write(subprocess.Popen(["git", "pull"], stdout = subprocess.PIPE).communicate()[0])
+except:
+    f.write("Error: " + sys.exc_info()[0])
+    f.write("Exiting.")
+    f.close()
