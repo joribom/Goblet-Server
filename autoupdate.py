c44640b9683f06b2fc00af09f36ff65627f2c9a0
Johan Ribom <jribom@kth.se>
2016-09-01 16:40:57 +0200

    Added timestamps to logging output.

--- autoupdate.py
+++ autoupdate.py
@@ -1,5 +1,16 @@
 #!/usr/bin/python
-import subprocess, time
+import subprocess, time, re, sys
+from time import gmtime, strftime
+
+def log(log_string):
+    with open("update.log", 'a') as f:
+        time = strftime("%Y-%m-%d %H:%M:%S", gmtime())
+        indent = len(time + " - ") * " "
+        strings = log_string.split("\n")
+        f.write(time + " - " + re.sub(r'([^\n])$', r'\1\n', strings[0]))
+        for s in strings[1:]:
+            f.write(time + " - " + re.sub(r'([^\n])$', r'\1\n', s))
+
 try:
     while True:
         subprocess.Popen(["git", "remote", "update"], stdout=subprocess.PIPE).communicate()[0]
@@ -7,17 +18,12 @@ try:
         remote = subprocess.Popen(["git", "rev-parse", r"@{u}"], stdout = subprocess.PIPE).communicate()[0]
         base = subprocess.Popen(["git", "merge-base", "@", r"@{u}"], stdout = subprocess.PIPE).communicate()[0]
         if local == remote:
-            f = open("update.log", 'a')
-            f.write("No updates.\n")
-            f.close()
+            log("No updates.")
             time.sleep(30)
         elif local == base:
-            f = open("update.log", 'a')
-            f.write("Pulling changes.\n")
-            f.write(subprocess.Popen(["git", "pull"], stdout = subprocess.PIPE).communicate()[0] + "\n")
-            f.close()
-except:
-    f = open("update.log", 'a')
-    f.write("Error: " + sys.exc_info()[0] + "\n")
-    f.write("Exiting.\n")
-    f.close()
+            log("Pulling changes.")
+            log(subprocess.Popen(["git", "pull"], stdout = subprocess.PIPE).communicate()[0])
+except Exception, e:
+    print e
+    log("Error: " + str(e) + "\n")
+    log("Exiting.\n")
