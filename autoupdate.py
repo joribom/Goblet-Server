f03dd217f31738b1add091f0338935feff1dfef8
Johan Ribom <jribom@kth.se>
2016-09-01 16:14:43 +0200

    Minor logging changes.

--- autoupdate.py
+++ autoupdate.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python
 import subprocess, time
-f = open("update.log", 'w')
 try:
     while True:
         subprocess.Popen(["git", "remote", "update"], stdout=subprocess.PIPE).communicate()[0]
@@ -9,11 +8,16 @@ try:
         base = subprocess.Popen(["git", "merge-base", "@", r"@{u}"], stdout = subprocess.PIPE).communicate()[0]
         if local == remote:
             time.sleep(30)
+            f = open("update.log", 'w')
             f.write("No updates.")
+            f.close()
         elif local == base:
+            f = open("update.log", 'w')
             f.write("Pulling changes.")
             f.write(subprocess.Popen(["git", "pull"], stdout = subprocess.PIPE).communicate()[0])
+            f.close()
 except:
+    f = open("update.log", 'w')
     f.write("Error: " + sys.exc_info()[0])
     f.write("Exiting.")
     f.close()
