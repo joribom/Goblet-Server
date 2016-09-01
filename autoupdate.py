08307dde3cb20ea051bd03b5f2565b59bf490ee9
Johan Ribom <jribom@kth.se>
2016-09-01 16:26:49 +0200

    Changed logging to append to logfile instead of overwriting it.

--- autoupdate.py
+++ autoupdate.py
@@ -7,17 +7,17 @@ try:
         remote = subprocess.Popen(["git", "rev-parse", r"@{u}"], stdout = subprocess.PIPE).communicate()[0]
         base = subprocess.Popen(["git", "merge-base", "@", r"@{u}"], stdout = subprocess.PIPE).communicate()[0]
         if local == remote:
-            time.sleep(30)
-            f = open("update.log", 'w')
+            f = open("update.log", 'a')
             f.write("No updates.\n")
             f.close()
+            time.sleep(30)
         elif local == base:
-            f = open("update.log", 'w')
+            f = open("update.log", 'a')
             f.write("Pulling changes.\n")
             f.write(subprocess.Popen(["git", "pull"], stdout = subprocess.PIPE).communicate()[0] + "\n")
             f.close()
 except:
-    f = open("update.log", 'w')
+    f = open("update.log", 'a')
     f.write("Error: " + sys.exc_info()[0] + "\n")
     f.write("Exiting.\n")
     f.close()
