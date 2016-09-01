3b01064df5dfb9ef5b3dc4b1c00569c3c8b8ed6a
Johan Ribom <jribom@kth.se>
2016-09-01 16:18:54 +0200

    Added some newlines to logging output.

--- autoupdate.py
+++ autoupdate.py
@@ -9,15 +9,15 @@ try:
         if local == remote:
             time.sleep(30)
             f = open("update.log", 'w')
-            f.write("No updates.")
+            f.write("No updates.\n")
             f.close()
         elif local == base:
             f = open("update.log", 'w')
-            f.write("Pulling changes.")
-            f.write(subprocess.Popen(["git", "pull"], stdout = subprocess.PIPE).communicate()[0])
+            f.write("Pulling changes.\n")
+            f.write(subprocess.Popen(["git", "pull"], stdout = subprocess.PIPE).communicate()[0] + "\n")
             f.close()
 except:
     f = open("update.log", 'w')
-    f.write("Error: " + sys.exc_info()[0])
-    f.write("Exiting.")
+    f.write("Error: " + sys.exc_info()[0] + "\n")
+    f.write("Exiting.\n")
     f.close()
